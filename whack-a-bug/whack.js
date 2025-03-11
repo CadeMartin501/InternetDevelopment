ae3eb6c31a778233d49e3f545075d92e936a18d7
cadeemartin <caderade88@gmail.com>
2025-03-11 13:42:25 -0500

    in class 3-11

---
+++ whack-a-bug/whack.js
@@ -0,0 +1,65 @@
+/*
+ * CSE 154 Section Exercise - Whack a Bug
+ * Handles whacking bugs.
+ */
+
+
+(function() {
+  'use strict';
+  window.addEventListener('load', init);
+
+  /**
+   * TODO
+   * Set up event listeners for the bugs.
+   */
+  function init() {
+    let antList = qsa('#bug-container>img');
+        for (let i=0; i<antList.length; i++) {
+            antList[i].addEventListener('click', whackBug);
+        }
+  }
+
+  /**
+   * TODO
+   * whacks the clicked bug and increments the score. The bug cannot be whacked again afterwards.
+   */
+  function whackBug(event) {
+    let clicked = event.currentTarget;
+    let scoreIncrease = document.getElementById('score');
+    if (!clicked.classList.contains('whacked')) {
+      scoreIncrease.textContent = parseInt(scoreIncrease.textContent) + 1;
+    }
+        clicked.classList.add('whacked');
+        clicked.src="bug-whacked.png";
+  }
+
+/* --- CSE 154 HELPER FUNCTIONS --- */
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} name - element ID.
+   * @returns {object} - DOM object associated with id.
+   */
+  function id(name) {
+    return document.getElementById(name);
+  }
+
+  /**
+   * Returns first element matching selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - DOM object associated selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns an array of elements matching the given query.
+   * @param {string} query - CSS query selector.
+   * @returns {array} - Array of DOM objects matching the given query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+
+})();
