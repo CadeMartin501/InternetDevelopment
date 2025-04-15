06a4475eb1026a5cacaa8a6ee1aea7b65bd62839
cadeemartin <caderade88@gmail.com>
2025-04-15 15:54:56 -0500

    worked on katego, need to finish still

--- katego/katego.js
+++ katego/katego.js
@@ -2,25 +2,64 @@
     "use strict";
 
     var rollActive = false;
+    var diceRoll1;
+    var diceRoll2;
+    var score;
 
     window.addEventListener('load', init);
 
     function init() {
         const rollButton = id('roll');
-        rollButton.addEventListener('click', rollDice);
+        const enterButton = qsa('.scoring-row > button');
+        //create event listener for all enter buttons.
+        for (var i = 0; i < enterButton.length; i ++) {
+            enterButton[i].addEventListener('click', buttonPress);
+        }
+        rollButton.addEventListener('click', rollPress);
     }
 
-    function rollDice() {
+    function rollPress() {
         const rollButton = id('roll');
-        if (!rollActive) {
-        var diceRoll1 = Math.floor(Math.random()*6+1);
-        var diceRoll2 = + Math.floor(Math.random()*6+1);
-        var score = diceRoll1 + diceRoll2
         const status = id('status');
-        status.textContent = `${diceRoll1} + ${diceRoll2} = ${score}`;
-        rollActive = true; //can't repress the roll button,
+        
+        if (!rollActive) {
+            diceRoll1 = dieRoll();
+            diceRoll2 = dieRoll();
+            score = diceRoll1 + diceRoll2;
+            status.textContent = `${diceRoll1} + ${diceRoll2} = ${score}`;
+            rollActive = true; //can't repress the roll button.
+        } else {
+            status.textContent += '!'; //need to change this later to make background flash red for 0.5 seconds.
         }
     }
+
+    function buttonPress(event) {
+        var enterButtonPressed = event.target.id;
+        const correspondingUserBox = id(`user${enterButtonPressed[5]}`);
+        if (rollActive && !correspondingUserBox.classList.contains('used')) {
+            correspondingUserBox.classList.add('used');
+            correspondingUserBox.value = score;
+        }
+        //Computer's turn.
+        var computerScore = dieRoll() + dieRoll();
+        console.log(computerScore);
+        const numPicked = Math.floor(Math.random()*5+1);
+        var boxPicked;
+        boxPicked = id(`computer${numPicked}`);
+        while (boxPicked.classList.contains('used')) {
+            numPicked = (numPicked + 1) % 5;
+            boxPicked = id(`computer${numPicked}`);
+        }
+        
+        boxPicked.classList.add('used');
+
+    }
+
+    function dieRoll() {
+        return Math.floor(Math.random()*6+1);
+    }
+
+
 
     /////////////////////////////////////////////////////////////////////
     // Helper functions
