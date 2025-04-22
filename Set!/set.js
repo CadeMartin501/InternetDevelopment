edff29e9a88111da6abcfbad8cad1b7c0e7b97f1
cadeemartin <caderade88@gmail.com>
2025-04-22 10:41:47 -0500

    worked on set!

--- Set!/set.js
+++ Set!/set.js
@@ -1,6 +1,9 @@
 (function() {
     'use strict';
 
+    let timerId;
+    let remainingSeconds;
+
     window.addEventListener('load', init);
 
     function init() {
@@ -8,6 +11,7 @@
         startButton.addEventListener('click', toggleView);
         let backButton = id('back-btn');
         backButton.addEventListener('click', toggleView);
+        remainingSeconds = qs('select').value;
 
     }
 
@@ -17,6 +21,58 @@
         let game = id('game-view');
         game.classList.toggle('hidden');
     }
+
+    /**
+ * Checks to see if the three selected cards make up a valid set. This is done by comparing each
+ * of the type of attribute against the other two cards. If each four attributes for each card are
+ * either all the same or all different, then the cards make a set. If not, they do not make a set
+ * @param {DOMList} selected - List of all selected cards to check if a set.
+ * @return {boolean} True if valid set false otherwise.
+ */
+function isASet(selected) {
+    let attributes = [];
+    for (let i = 0; i < selected.length; i++) {
+      attributes.push(selected[i].id.split("-"));
+    }
+    for (let i = 0; i < attributes[0].length; i++) {
+      let allSame = attributes[0][i] === attributes[1][i] &&
+                    attributes[1][i] === attributes[2][i];
+      let allDiff = attributes[0][i] !== attributes[1][i] &&
+                    attributes[1][i] !== attributes[2][i] &&
+                    attributes[0][i] !== attributes[2][i];
+      if (!(allDiff || allSame)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+/**Returns a randomly-generated array of string attributes in the form [COLOR, FILL, SHAPE, COUNT]
+ * @param {boolean} isEasy - If the game mode is set to easy or not
+ */
+  function generateRandomAttributes(isEasy) {
+
+  }
+
+//Return a div element with COUNT number of img elements appended as children
+  function generateUniqueCard(isEasy) {
+
+  }
+
+//Starts the timer for a new game. No return value.
+  function startTimer() {
+
+  }
+
+//Updates the game timer (module-global and #time shown on page) by 1 second. No return value.
+  function advanceTimer() {
+
+  }
+
+//Used when a card is selected, checking how many cards are currently selected. If 3 cards are selected, uses isASet (provided) to handle "correct" and "incorrect" cases. No return value.
+  function cardSelected() {
+
+  }
 
     /////////////////////////////////////////////////////////////////////
     // Helper functions
