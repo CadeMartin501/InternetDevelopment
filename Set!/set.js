5bc83e1c717f07412bbd60ce8d3c84251e4de37b
cadeemartin <caderade88@gmail.com>
2025-04-24 11:00:26 -0500

    generateCards and cardSelected

--- Set!/set.js
+++ Set!/set.js
@@ -21,8 +21,18 @@
       menu.classList.toggle('hidden');
       let game = id('game-view');
       game.classList.toggle('hidden');
-      let isEasy =
-      console.log(generateRandomAttributes(false));
+      const board = id('board');
+      for (let i = 0; i<12; i++) {
+        let card = generateUniqueCard(false);
+        card.classList.add('card');
+        card.addEventListener('click', cardSelected);
+        let shape = document.createElement('img');
+        shape.src = `img/${card.classList[0]}-${card.classList[1]}-${card.classList[2]}.png`;
+        for (let i = 0; i< card.classList[3]; i++) {
+        card.appendChild(shape);
+        }
+        board.appendChild(card);
+      }
     }
 
     /**
@@ -49,6 +59,7 @@
       }
     }
     remainingSeconds = remainingSeconds+15;
+    setCount++;
     return true;
     }
 
@@ -56,36 +67,47 @@
  * @param {boolean} isEasy - If the game mode is set to easy or not
  */
   function generateRandomAttributes(isEasy) {
-    let colors = ["Red", "Purple", "Green"];
-    let fills = ["Outline", "Solid", "Striped"];
-    let shapes = ["Diamond", "Oval", "Squiggle"];
-    let counts = ["One", "Two", "Three"];
-    if (!isEasy) {
+    let colors = ["red", "purple", "green"];
+    let fills = ["outline", "solid", "striped"];
+    let shapes = ["diamond", "oval", "squiggle"];
+    let counts = [1, 2, 3];
+    
     return [colors[Math.floor(Math.random()*colors.length)], fills[Math.floor(Math.random()*fills.length)], shapes[Math.floor(Math.random()*shapes.length)], counts[Math.floor(Math.random()*counts.length)]]
-    } else {
-      return [colors[Math.floor(Math.random()*colors.length)], shapes[Math.floor(Math.random()*shapes.length)], counts[Math.floor(Math.random()*counts.length)]]
-    }
   }
 
 //Return a div element with COUNT number of img elements appended as children
   function generateUniqueCard(isEasy) {
+    let attributes = generateRandomAttributes(isEasy);
+    let newCard = document.createElement('div');
+    newCard.classList.add(attributes[0]); //Color
+    newCard.classList.add(attributes[1]); //Fill
+    newCard.classList.add(attributes[2]); //Shape
+    newCard.classList.add(attributes[3]); //Count
 
+    if (isEasy) {
+      newCard.classList.remove(attributes[1]);
+      newCard.classList.add('Solid');
+       }
+
+    return newCard;
+    // let cards = qsa('board > div');
   }
 
 //Starts the timer for a new game. No return value.
   function startTimer() {
     timerId = qs('select');
     remainingSeconds= timerId.value;
+    setInterval(advanceTimer, 1000);
   }
 
 //Updates the game timer (module-global and #time shown on page) by 1 second. No return value.
   function advanceTimer() {
-
+    remainingSeconds--;
   }
 
 //Used when a card is selected, checking how many cards are currently selected. If 3 cards are selected, uses isASet (provided) to handle "correct" and "incorrect" cases. No return value.
   function cardSelected() {
-
+    this.classList.add('selected');
   }
 
     /////////////////////////////////////////////////////////////////////
@@ -99,18 +121,18 @@
     *                    Promise result
     */
     async function statusCheck(res) {
-        if (!res.ok) {
-            throw new Error(await res.text());
-        }
-        return res;
+      if (!res.ok) {
+          throw new Error(await res.text());
+      }
+      return res;
     }
 
     function id(id) {
-        return document.getElementById(id);
+      return document.getElementById(id);
     }
 
     function qs(selector) {
-        return document.querySelector(selector);
+      return document.querySelector(selector);
     }
 
     function qsa(selector) {
