47cf76b5b7c5f6cd1505744173e04a53007b024d
CadeDesktop <caderade88@gmail.com>
2025-05-05 11:14:22 -0500

    need to get key from seperate file

---
+++ APICreativeProject/Key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    UVKey: openuv-xmnxurmab7ct5m-io
+};
---
+++ APICreativeProject/UV.js
@@ -0,0 +1,43 @@
+const { UVKey } = require("./Key.js");
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', init);
+
+    // Import the API key from another file.
+    const key = require('./apikey.js');
+
+    function init() {
+        console.log(UVKey);
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    // Helper functions
+    /**
+    * Helper function to return the response's result text if successful, otherwise
+    * returns the rejected Promise result with an error status and corresponding text
+    * @param {object} res - response to check for success/error
+
+    * @return {object} - valid response if response was successful, otherwise rejected
+    *                    Promise result
+    */
+    async function statusCheck(res) {
+        if (!res.ok) {
+            throw new Error(await res.text());
+        }
+        return res;
+    }
+
+    function id(id) {
+        return document.getElementById(id);
+    }
+
+    function qs(selector) {
+        return document.querySelector(selector);
+    }
+
+    function qsa(selector) {
+        return document.querySelectorAll(selector);
+    }
+})();
